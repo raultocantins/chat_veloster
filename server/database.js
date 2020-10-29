bbd0a47cb47692e8c38dee33b6fad5dddcdd11c0
raultocantins <alexbraul.ar@gmail.com>
2020-10-29 11:08:48 -0300

    Alterando frontend e backend para produção

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose')
 
-var connection = mongoose.connect('mongodb+srv://admin:3571592486@cluster0.ebc1x.mongodb.net/Users?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true })
+var connection = mongoose.connect('mongodb+srv://<user>:<password>@cluster0.ebc1x.mongodb.net/<database>?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true })
 if(connection){
     console.log("Conexão ao mongodb")
 }else{
